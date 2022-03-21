0902ced3d29546232b1aeb6e46087658936b7ee7
Kitsune-fx <kant.cmd@gmail.com>
2022-03-21 13:44:12 +0700

    [Forgot to ignore .env]

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.use((req, res, next) => {
     res.type('json').send({ "message": "404 file not found" });
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
